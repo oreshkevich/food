6824276811d3307cbcae504b53ea3f2c719e1d22
Henadzi Arashkevich <oreshkevich85@mail.ru>
2021-11-07 22:23:39 +0300

    feat: add swiper

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// class Singleton {
-//   constructor() {
-//     return (Singleton.a = Singleton.a ? Singleton.a : this);
-//   }
-// }
+// let str = "abcde";
+// let sub = str.slice(1, 3);
+// let s = str.substring(1, 3);
+// let subv = str.substr(1, 2);
+
+// console.log(sub);
+// console.log(s);
+// console.log(subv);
